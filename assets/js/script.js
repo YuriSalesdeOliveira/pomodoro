4acdc499c715cffc83954730cd7baca58d97372d
YuriSalesdeOliveira <yuri_oli@hotmail.com>
2021-12-07 01:31:45 -0300

    Feature: choose timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,41 @@
-function init()
-{
-    function Timer(hours, minutes, seconds) {
 
-        this.hours = hours
-        this.minutes = minutes
-        this.seconds = seconds
+function Timer(timer_option = 'work_time') {
 
-        this.interval = null
-
-        this.reset = function () {
-            this.hours = hours
-            this.minutes = minutes
-            this.seconds = seconds
-        }
+    this.timer_options = {
+        work_time: {hours: '00', minutes: '25', seconds: '00'},
+        break_time: {hours: '00', minutes: '00', seconds: '02'},
+        long_break_time: {hours: '00', minutes: '10', seconds: '00'}
     }
 
-    const timer = new Timer('00', '25', '00')
-    
-    insertTime(timer)
+    let time = this.timer_options[timer_option]
 
-    const button_start = document.querySelector('.start')
-    const button_pause = document.querySelector('.pause')
-    const button_stop = document.querySelector('.stop')
+    this.hours = time.hours
+    this.minutes = time.minutes
+    this.seconds = time.seconds
 
-    button_start.addEventListener('click', () => { start(timer) })
-    button_pause.addEventListener('click', () => { pause(timer) })
-    button_stop.addEventListener('click', () => { stop(timer) })
+    this.interval = null
 
-    return timer
+    this.reset = function (timer_option = null) {
+        if (timer_option) time = this.timer_options[timer_option]
+        this.hours = time.hours
+        this.minutes = time.minutes
+        this.seconds = time.seconds
+    }
 }
 
-function start(timer)
+const timer = new Timer()
+
+insertTime(timer)
+
+const button_start = document.querySelector('.start')
+const button_pause = document.querySelector('.pause')
+const button_stop = document.querySelector('.stop')
+
+button_start.addEventListener('click', () => { start() })
+button_pause.addEventListener('click', () => { pause() })
+button_stop.addEventListener('click', () => { stop() })
+
+function start()
 {
     function subtractOneSecond(timer)
     {
@@ -51,12 +56,12 @@ function start(timer)
     }, 1000)
 }
 
-function pause(timer)
+function pause()
 {
     clearInterval(timer.interval)
 }
 
-function stop(timer)
+function stop()
 {
     clearInterval(timer.interval)
     timer.reset()
@@ -71,55 +76,61 @@ function createButton(button_content)
     return button
 }
 
-function finish(timer, container_timer_class = 'pomodoro_timer',
-    where_insert_button_for_stop_alert = 'pomodoro_actions')
-{   
-    function alertEndTimer(where_insert_button_class,
-        src = 'https://toqueparacelular.com/download/?id=1006&post=1005')
-    {
-        let audio = new Audio(src)
-        audio.play()
-
-        button_audio_stop = createButton('stop alert')
-
-        let where_insert_button = document.querySelector(`.${where_insert_button_class}`)
-        where_insert_button.appendChild(button_audio_stop)
-
-        button_audio_stop.addEventListener('click', () => {
-
-            audio.pause()
-
-            where_insert_button.removeChild(button_audio_stop)
-        })
-    }
-
-    function handleMutationObserver( mutations ) {
-
-        mutations.forEach(function(mutation) {
-            
-            if (mutation.target.innerHTML === '00:00:00') {
-
-                alertEndTimer(where_insert_button_for_stop_alert)
-
-                stop(timer)
-            }
-        })
-    }
-
-    let observer = new MutationObserver( handleMutationObserver )
-
-    let target = document.querySelector(`.${container_timer_class}`)
-    let config = { childList: true }
-  
-    observer.observe( target, config )
-
-}
-
 function insertTime({ hours, minutes, seconds }, container_timer_class = 'pomodoro_timer')
 {
     let container_timer = document.querySelector(`.${container_timer_class}`)
     container_timer.innerHTML = `${hours}:${minutes}:${seconds}`
 }
 
-let timer = init()
-finish(timer)
+let timer_options = document.querySelectorAll('.timer_option')
+
+timer_options.forEach((option) => {
+
+    option.addEventListener('click', () => {
+
+        let timer_option = option.getAttribute('data-timer_option')
+        timer.reset(timer_option)
+        insertTime(timer)
+
+    })
+})
+
+function alertEndTimer(where_insert_button_class,
+    src = 'https://toqueparacelular.com/download/?id=1006&post=1005')
+{
+    let audio = new Audio(src)
+    audio.play()
+
+    button_audio_stop = createButton('stop alert')
+
+    let where_insert_button = document.querySelector(`.${where_insert_button_class}`)
+    where_insert_button.appendChild(button_audio_stop)
+
+    button_audio_stop.addEventListener('click', () => {
+
+        audio.pause()
+
+        where_insert_button.removeChild(button_audio_stop)
+    })
+}
+
+function handleMutationObserver( mutations ) {
+
+    mutations.forEach(function(mutation) {
+        
+        if (mutation.target.innerHTML === '00:00:00') {
+
+            stop(timer)
+
+            alertEndTimer('pomodoro_actions')
+            
+        }
+    })
+}
+
+let observer = new MutationObserver( handleMutationObserver )
+
+let target = document.querySelector('.pomodoro_timer')
+let config = { childList: true }
+
+observer.observe( target, config )
